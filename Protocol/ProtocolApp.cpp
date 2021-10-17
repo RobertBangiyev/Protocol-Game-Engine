8ac9efe1b1953d575d8f48e98eccb985070b678a
Robert Bangiyev <rbangiyev123@gmail.com>
2021-10-17 15:07:35 -0400

    intro to glfw

--- Protocol/ProtocolApp.cpp
+++ Protocol/ProtocolApp.cpp
@@ -1,11 +1,19 @@
 #include "pch.h"
 #include "ProtocolApp.h"
+#include <GLFW/glfw3.h>
 
 namespace Protocol {
 	void ProtocolApp::Run() {
 		std::cout << "Protocol is running..." << std::endl;
+
+		GLFWwindow* window;
+		glfwInit();
+		window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
+
 		while (true) {
 			OnUpdate();
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
 	}
 
